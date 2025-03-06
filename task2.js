27ece967913d61d71dada44e9d12a8d44841af45
Mustafa Eissa <128483502+MuEissa@users.noreply.github.com>
2025-03-06 22:06:48 +0100

    Delete task2.js

--- task2.js
+++
@@ -1,8 +0,0 @@
-// JavaScript
-window.addEventListener('scroll', () => {
-    const scrollY = window.scrollY;
-    document.querySelector('.background').style.transform = `translateY(${scrollY * 0.5}px)`;
-    document.querySelector('.midground').style.transform = `translateY(${scrollY * 0.3}px)`;
-    document.querySelector('.foreground').style.transform = `translateY(${scrollY * 0.1}px)`;
-    // Add more interactive effects here
-  });
